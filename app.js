5f13ac74a3afd56649afa693b0fed4f1152b15b3
Michael Ramos <Michael.aramos91@gmail.com>
2020-11-22 11:37:15 -0800

    Added jokes to jokes array

---
+++ app.js
@@ -0,0 +1,7 @@
+//Array of Knock Knock Jokes
+const jokes = [
+  "Broccoli? Broccoli who? Broccoli doesn't have a last name silly",
+  "Amish. Amish who? Really? You don't look like a shoe!",
+  "A little old lady. A little old lady who? I didn't know you could yodel!",
+  "Mikey. Mikey who? Mike doesn't fit in the key hole!",
+];
